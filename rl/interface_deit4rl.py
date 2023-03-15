6773b52dec622910f20af7d8389154074de45298
LuErShuai <lu1192213934@outlook.com>
2023-03-15 07:06:01 -0400

    add rl algorithm

---
+++ rl/interface_deit4rl.py
@@ -0,0 +1,9 @@
+
+Class MultOutputHead():
+    def __init__(self):
+        pass
+
+    
+Class MultiOutpuHeadAuto():
+    def __init__(self):
+        pass
---
+++ rl/interface_rl4deit.py
@@ -0,0 +1,19 @@
+import torch
+from rl.actor_critic import Agent
+
+
+Class Interface():
+    def __init__(self):
+        self.agent = Agent()
+        pass
+
+    def agent_startup():
+        pass
+
+    def send_state_to_agent():
+        pass
+
+    def get_deactivate_policy_from_agent():
+        pass
+
+
